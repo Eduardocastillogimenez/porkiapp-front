6da2c7d08c0f446db60ad1d93a3fd19bbe81ab33
Eduardo_cas123 <eduardocastillogimenez1@gmail.com>
2025-02-12 23:05:18 -0400

    .

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
-import App from "./app.jsx";
+import App from "./App.jsx";
+import Dasboards from "./views/dasboards";
+
 
 // createRoot(document.getElementById('root')).render()
 
@@ -17,6 +19,11 @@ ReactDOM.createRoot(root).render(
       <Route path="/" element={
          <StrictMode>
           <App />
+        </StrictMode>
+      } />
+      <Route path="/dasboards" element={
+         <StrictMode>
+          <Dasboards />
         </StrictMode>
       } />
     </Routes>
